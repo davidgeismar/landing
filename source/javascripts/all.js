bd73ff0751bb74bc5a7114275ca22aa92d6c215c
David Geismar <davidgeismar@hotmail.fr>
2015-03-19 23:29:37 +0100

    landing_page_wefooot

---
+++ source/javascripts/all.js
@@ -0,0 +1,21 @@
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree .
+
+ $( "#home_page" ).fadeOut( "slow", function() {
+    // Animation complete.
+  });
+
+  $( "#page_trouver" ).fadeIn( "slow", function() {
+    // Animation complete
+  });
+
+  $( "#page_trouver" ).fadeOut( "slow", function() {
+    // Animation complete
+  });
+ $( "#page_defi" ).fadeIn( "slow", function() {
+    // Animation complete
+  });
+  $( "#home_page" ).fadeOut( "slow", function() {
+    // Animation complete.
+  });
