e68cedd272b602f6a2516abec25ec19fa2b7d285
David Geismar <davidgeismar@hotmail.fr>
2015-03-20 18:49:58 +0100

    animations and caroussel

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,21 +1,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+//= require index
+//= require scrollReveal
 
- $( "#home_page" ).fadeOut( "slow", function() {
-    // Animation complete.
-  });
 
-  $( "#page_trouver" ).fadeIn( "slow", function() {
-    // Animation complete
-  });
-
-  $( "#page_trouver" ).fadeOut( "slow", function() {
-    // Animation complete
-  });
- $( "#page_defi" ).fadeIn( "slow", function() {
-    // Animation complete
-  });
-  $( "#home_page" ).fadeOut( "slow", function() {
-    // Animation complete.
-  });
