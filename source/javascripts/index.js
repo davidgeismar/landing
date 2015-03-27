675b590ac8d99071a6291f451aa4cc8e5d356331
David Geismar <davidgeismar@hotmail.fr>
2015-03-27 16:50:16 +0100

    landing ok for safari

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,16 +1,21 @@
   setTimeout(
     function() {
       document.getElementById("hide").style.visibility = "visible";
-    }, 3000);
+    }, 4000);
 
   setTimeout(
     function() {
       document.getElementById("hide2").style.visibility = "visible";
-    }, 7000);
+    }, 8500);
 
   setTimeout(
     function() {
       document.getElementById("hide3").style.visibility = "visible";
-    }, 11000);
+    }, 12500);
+
+   setTimeout(
+    function() {
+      document.getElementById("hide4").style.visibility = "visible";
+    }, 16500);
 
 
