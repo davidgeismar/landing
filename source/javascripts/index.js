0bdd8914dece2ed2c136c7e39a5080ee166bfbb7
David Geismar <davidgeismar@hotmail.fr>
2015-04-12 13:11:51 +0300

    found responsive prob

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,22 +1,3 @@
-  // setTimeout(
-  //   function() {
-  //     document.getElementById("hide").style.visibility = "visible";
-  //   }, 4000);
-
-  // setTimeout(
-  //   function() {
-  //     document.getElementById("hide2").style.visibility = "visible";
-  //   }, 8500);
-
-  // setTimeout(
-  //   function() {
-  //     document.getElementById("hide3").style.visibility = "visible";
-  //   }, 12500);
-
-  //  setTimeout(
-  //   function() {
-  //     document.getElementById("hide4").style.visibility = "visible";
-  //   }, 16500);
 
 
 $('.carousel').carousel({
